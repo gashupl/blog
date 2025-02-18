29f2181f7302310d3a9c73ac81b5e12a43a499ff
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2025-02-18 08:31:08 +0100

    Add Section model and repository, update Syncfusion package

---
+++ SfGridCustomButtons/src/Pg.SfGridCustomButtons.Client/Data/Section.cs
@@ -0,0 +1,9 @@
+﻿namespace Pg.SfGridCustomButtons.Client.Data
+{
+    public class Section
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ SfGridCustomButtons/src/Pg.SfGridCustomButtons.Client/Data/SectionRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Pg.SfGridCustomButtons.Client.Data
+{
+    public class SectionRepository
+    {
+        private List<Section> _sections; 
+        public SectionRepository() 
+        {
+            _sections = new List<Section>()
+            {
+                new Section() {Id = 1, Name = "Section 1", Description = "Description 1"},
+                new Section() {Id = 2, Name = "Section 2", Description = "Description 2"},
+                new Section() {Id = 3, Name = "Section 3", Description = "Description 3"},
+                new Section() {Id = 4, Name = "Section 4", Description = "Description 4"},
+                new Section() {Id = 5, Name = "Section 5", Description = "Description 5"}
+            }; 
+        }
+
+        public List<Section> GetAll()
+        {
+            return _sections;
+        }
+    }
+}
